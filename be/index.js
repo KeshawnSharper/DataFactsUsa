f9118aa0a899ff0eac787bf39b182db2b788e16f
KeshawnSharper <ksharper@studentmba.org>
2021-05-06 07:28:51 -0400

    Installed mocha and chai then tested first endpoint

--- be/index.js
+++ be/index.js
@@ -17,3 +17,5 @@ server.listen(PORT, () => {
     
   console.log(`Listening on port ${PORT}...`);
 });
+
+module.exports = server
---
+++ be/test/server.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+var server = require( '../index' )
+
+describe("Server!", () => {
+    it("welcomes user to the api", done => {
+      chai
+        .request(server)
+        .get("/")
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        })
+    })
+})
